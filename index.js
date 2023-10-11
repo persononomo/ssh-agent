8f37c195dad623fc8caf6bad87088d8680ed5876
Pavlo Kozub <pavlo.kozub@gmail.com>
2023-10-11 18:52:52 +0300

    add some logs

--- index.js
+++ index.js
@@ -54,11 +54,11 @@ try {
     console.log('Configuring deployment key(s)');
 
     child_process.execFileSync(sshAddCmd, ['-L']).toString().trim().split(/\r?\n/).forEach(function(key) {
-        core.info(key);
-        core.info(key.length);
+        console.log(key);
+        console.log(key.length);
         const parts = key.match(/\bgithub\.com[:/]([_.a-z0-9-]+\/[_.a-z0-9-]+)/i);
 
-        core.info(parts);
+        console.log(parts);
         if (!parts) {
             if (logPublicKey) {
               console.log(`Comment for (public) key '${key}' does not match GitHub URL pattern. Not treating it as a GitHub deploy key.`);
